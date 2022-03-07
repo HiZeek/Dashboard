605ca22c009107827a2d4e42e531c132cfdbbe4e
HiZeek <idibiaisaac3@gmail.com>
2022-03-07 13:32:05 +0100

    complete dashboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,36 @@
 //Toggle mobile menu
+const menuToggleOpen = document.querySelector('.menu-toggle-open');
+const menuToggleClose = document.querySelector('.menu-toggle-close');
+const navMobileElement = document.querySelector('.nav-mobile');
+
+menuToggleOpen.addEventListener('click', () => navMobileElement.classList.toggle('active'));
+menuToggleClose.addEventListener('click', () => navMobileElement.classList.remove('active'));
+
 
 //Toggle theme
+const bodyElement = document.body;
+const themeToggleBtn = document.querySelector('.theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+
+if (currentTheme) {
+    bodyElement.classList.add('theme-light');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    bodyElement.classList.toggle('theme-light');
+    if(bodyElement.classList.contains('theme-light')) {
+        localStorage.setItem('currentTheme', 'themeActive');
+    } else {
+        localStorage.removeItem('currentTheme', 'themeActive');
+    }
+})
 
 //Show dropdown menu
+document.addEventListener('click', (e) => {
+    const dropdown = document.querySelector('.dropdown');
+    if(e.target.classList.contains('dropdown-btn')) {
+        dropdown.classList.add('active');
+    } else {
+        dropdown.classList.remove('active');
+    }
+});
